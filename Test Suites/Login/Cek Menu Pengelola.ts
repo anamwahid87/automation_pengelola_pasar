87e0bbbea1e875738a2d89bc0f00587af9b0b5d0
anamuno <anamwahid87@gmail.com>
2021-02-10 13:32:03 +0700

    add new Test Suite

---
+++ Test Suites/Login/Cek Menu Pengelola.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Cek Menu Pengelola</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>3916e37f-6c1b-47bb-862e-9374456e734d</testSuiteGuid>
+   <testCaseLink>
+      <guid>ae6a31b4-cb43-4c34-b17f-522120f83379</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Cek Menu/Menu Pengelola Jatijajar</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
